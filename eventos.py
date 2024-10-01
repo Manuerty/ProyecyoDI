b3b578a9de8dfc3a641be1060ebfdc34adcb1ad6
a23manuelcr <manuelcendonr@gmail.com>
2024-10-01 08:26:33 +0200

    feat(eventos.py): add function validarDNI

--- eventos.py
+++ eventos.py
@@ -33,3 +33,34 @@ class Eventos():
         var.ui.cmbMuniCli.clear()
         listado = conexion.Conexion.listaMuni(self)
         var.ui.cmbMuniCli.addItems(listado)
+
+    def validarDNIcli(dni):
+        try:
+            dni = str(dni).upper()
+            var.ui.txtDni.setText(str(dni))
+            tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+            dig_ext = "XYZ"
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = "1234567890"
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
+                    var.ui.lbldnicli.setStyleSheet('color:green;')  # si es válido se pone una V en color verde
+                    var.ui.lbldnicli.setText('V')
+                    return True
+                else:
+                    var.ui.lbldnicli.setStyleSheet('color:red;')  # y si no un aspa en color rojo
+                    var.ui.lbldnicli.setText('X')
+                    var.ui.txtdnicliente.setText(None)
+                    var.ui.txtdnicliente.setFocus()
+            else:
+                var.ui.lbldnicli.setStyleSheet('color:red;')
+                var.ui.lbldnicli.setText('X')
+                var.ui.txtdnicliente.setText(None)
+                var.ui.txtdnicliente.setFocus()
+
+        except Exception as error:
+            print("error en validar dni ", error)
