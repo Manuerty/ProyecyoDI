099205b77b15c1b223539ff3e348f12291f36b4e
a23manuelcr <manuelcendonr@gmail.com>
2024-10-29 09:09:30 +0100

    feat(propiedades): add error message

--- propiedades.py
+++ propiedades.py
@@ -1,6 +1,6 @@
 import conexion
 import var
-
+from PyQt6 import QtWidgets, QtGui, QtCore
 
 class Propiedades():
 
@@ -8,9 +8,21 @@ class Propiedades():
         try:
             tipo = var.dlggestion.ui.txtTipoProp.text().title()
             registro = conexion.Conexion.altaTipoProp(tipo)
-            var.ui.cmbTipoProp.clear()
-            var.ui.cmbTipoProp.addItems(registro)
-            var.dlggestion.ui.txtTipoProp.setText('')
+            if registro:
+                var.ui.cmbTipoProp.clear()
+                var.ui.cmbTipoProp.addItems(registro)
+                var.dlggestion.ui.txtTipoProp.setText('')
+            else:
+                mbox = QtWidgets.QMessageBox()
+                mbox.setIcon(QtWidgets.QMessageBox.Icon.Critical)
+                mbox.setWindowIcon(QtGui.QIcon("./img/icono.svg"))
+                mbox.setWindowTitle('Aviso')
+                mbox.setText("Ya existe ese tipo en la BBDD")
+                mbox.setStandardButtons(
+                    QtWidgets.QMessageBox.StandardButton.Ok)
+                mbox.setDefaultButton(QtWidgets.QMessageBox.StandardButton.Ok)
+                mbox.button(QtWidgets.QMessageBox.StandardButton.Ok).setText('Aceptar')
+                mbox.exec()
         except Exception as error:
             print('Error altaTipoPropiedad: %s ' % str(error))
 
